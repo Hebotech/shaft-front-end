57508c1aa9378edb6f2040c1deee66020861b019
IrfDev <irving.dev@icloud.om>
2020-04-19 03:22:18 -0500

    changing webpack

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
-const PreloadWebpackPlugin = require('preload-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const PreloadWebpackPlugin = require('preload-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    configureWebpack: {
-        plugins: [
-            new HtmlWebpackPlugin(),
-            new PreloadWebpackPlugin()
-        ]
-    },
+    // configureWebpack: {
+    //     plugins: [
+    //         new HtmlWebpackPlugin(),
+    //         new PreloadWebpackPlugin()
+    //     ]
+    // },
     css: {
         loaderOptions: {
             sass: {
