4756fe252d2a3ec365be2d894345272829f73670
IrfDev <irving.dev@icloud.om>
2020-04-19 01:54:13 -0500

    s

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,18 @@
+// const PreloadWebpackPlugin = require('preload-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     css: {
         loaderOptions: {
             sass: {
                 prependData: `@import "@/styles/_variables.scss";`
             }
-        }
+        },
+    },
+    // configureWebpack: {
+    //     plugins: [
+    //         new HtmlWebpackPlugin(),
+    //         new PreloadWebpackPlugin()
+    //     ]
+    // }
     }
-};
